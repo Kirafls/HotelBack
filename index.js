d9a65718374756340579dcc432da97e04fbffbb0
JesusRM04 <jaromomendez@gmail.com>
2023-06-16 17:49:14 -0600

    Correo para contacto

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
+let cors = require('cors');
+const bodyparser= require('body-parser');
 var PORT=3000;
+
+app.use(cors());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(require('./routes/correoRoutes'))
 app.get('/', function(req, res) {
   res.send('Hola Mundo!');
 });
